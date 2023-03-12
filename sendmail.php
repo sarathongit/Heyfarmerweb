f947c007ed17fde6be2fdb8af8f318f2e6d9b500
Sarath Krishna TK <86192772+sarathongit@users.noreply.github.com>
2023-03-12 12:30:13 +0530

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,13 +1,42 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $name = $_POST['name'];
   $email = $_POST['email'];
   $subject = $_POST['subject'];
   $message = $_POST['message'];
-  $to = 'teamfabd@gmail.com';
-  $body = "Name: $name\nEmail: $email\nSubject: $subject\n\n$message";
-  $headers = "From: $email\r\nReply-To: $email\r\n";
-  mail($to, $subject, $body, $headers);
-  echo 'Thank you for your message!';
+  
+  $mail = new PHPMailer(true);
+  try {
+    //Server settings
+    $mail->SMTPDebug = 0;                     //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'mailteamfabd@gmail.com';                  //SMTP username
+    $mail->Password   = 'FABD@2021';                    //SMTP password
+    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set 'tls' above
+
+    //Recipients
+    $mail->setFrom($email, $name);
+    $mail->addAddress('teamfabd@gmail.com');     //Add a recipient
+
+    //Content
+    $mail->isHTML(false);                                  //Set email format to plain text
+    $mail->Subject = $subject;
+    $mail->Body    = $message;
+
+    $mail->send();
+    echo 'Thank you for your message!';
+  } catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+  }
 }
 ?>
